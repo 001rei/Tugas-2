61e62e559185655f0fcc05b1884b9bcb8f8ee197
001rei <113325926+001rei@users.noreply.github.com>
2022-09-24 21:06:47 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+# TUGAS PRAKTIKUM
+
+x = 0
+jumlah = 0
+while x < 1000 :
+    if x%3 == 0 or x%5 == 0:
+        jumlah = jumlah + x
+        print(x)
+    x += 1
+
+print(jumlah)
+
+# NAMA : MUHAMMAD WILDAN ARYA ADIWIGUNA
+# NIM  : 220535609939
+     
